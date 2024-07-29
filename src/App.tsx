1ac0440d4cdb55cc23e999ad767ab28b13260eb4
Shub-ham <shub.shukla@protonmail.com>
2024-07-29 22:53:48 +0530

    Loading... text added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Calendar from "react-calendar";
 
 function App() {
   const [holidayData, setHolidayData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     handleDateChange(new Date());
@@ -15,10 +16,15 @@ function App() {
     const url = `${baseApiUrl}&year=${date.getFullYear()}&month=
     ${date.getMonth() + 1}&day=${date.getDate()}`;
 
+    setIsLoading(true);
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setHolidayData(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
       });
   };
 
@@ -27,17 +33,21 @@ function App() {
       <h2>Select the date to see the holiday</h2>
       <Calendar onChange={handleDateChange} />
 
-      <div className="holiday-data">
-        {holidayData.length ? (
-          holidayData.map((data: any, index: number) => (
-            <p className="holiday">
-              {index + 1}. {data.name}
-            </p>
-          ))
-        ) : (
-          <p className="no-holiday">No holiday found</p>
-        )}
-      </div>
+      {!isLoading && (
+        <div className="holiday-data">
+          {holidayData.length ? (
+            holidayData.map((data: any, index: number) => (
+              <p className="holiday">
+                {index + 1}. {data.name}
+              </p>
+            ))
+          ) : (
+            <p className="no-holiday">No holiday found</p>
+          )}
+        </div>
+      )}
+
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
